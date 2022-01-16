20d2b1419398e6717506edfc87d19665746ec18d
j.kunciene <j.kunciene@gmail.com>
2022-01-16 14:28:56 +0200

    first commit 18:30

---
+++ src/components/Balance.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <>
+           <h4>Your Balance</h4>
+           <h2>$ 00,00</h2>
+        </>
+    )
+}
+
+export default Balance
